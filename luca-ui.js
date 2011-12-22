6ffdcba79567dceda28c56e4874dac24ea8c9ee6
Jonathan Soeder <jonathan.soeder@gmail.com>
2011-12-21 23:05:12 -0600

    clean start

--- luca-ui.js
+++
@@ -1,5 +0,0 @@
-//= require ./luca-ui/framework
-//= require backbone_init
-//= require_tree ./luca-ui/core
-//= require_tree ./luca-ui/templates
-//= require_tree ./luca-ui/containers
--- luca-ui/containers/modal_view.coffee
+++
@@ -1,4 +0,0 @@
-Luca.ModalView = Luca.Container.extend 
-  className: 'luca-ui-modal-view'
-
-  components:[]
