a67a1c07ec4a715de52b436ce6f00009f65e5a2d
Alex Smith <aosmith@gmail.com>
2012-12-11 16:43:19 -0600

    WIP moving some static functionality and vars to module constants

---
+++ lib/luca/documentation/component_documentation.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/env ruby
+
+if __FILE__==$0
+  require 'pry'
+  require './lib/luca/documentation/documentation.rb'
+end
+
+module Luca
+  module Documentation
+    class ComponentDocumentation
+      attr_accessor :comments
+      attr_accessor :arguments
+      attr_accessor :method
+
+      def initialize component_name
+        @component_class_path = component_name.split('.')
+        read_file
+      end
+
+      def method_data_for method
+        @method = method
+        load_section
+        load_comments
+        load_arguments
+        self
+      end
+
+      def all
+        { comments:@comments, arguments:@arguments, method:@method }
+      end
+
+      private
+
+      def load_comments
+        unless @relevent_section.nil?
+          @comments = @relevent_section[0].match(Luca::Documentation::COMMENTS_REGEX)[0]
+        end
+      end
+
+      def load_arguments
+        args = @relevent_section[0].match(/^\s*#{@method}\s*:\s*\((.*)\)\s*-\>.*$/)[1].gsub(/\s/,'').split(',')
+        @arguments = args.inject({}) do |memo, arg|
+          if default_args = arg.match(Luca::Documentation::ARGUMENTS_REGEX)
+            memo[default_args[1].to_sym] = default_args[2]
+          else
+            memo[arg.to_sym] = nil
+          end
+          memo
+        end
+      end
+
+      def read_file
+        file_path = Luca::Documentation.find_file @component_class_path.last
+        @file_contents = File.open(file_path).read()
+      end
+
+      def underscore string
+        string.gsub(/::/, '/').
+        gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+        gsub(/([a-z\d])([A-Z])/,'\1_\2').
+        tr("-", "_").
+        downcase
+      end
+
+      def load_section
+        @relevent_section = @file_contents.match(/(^\s*#.*$\n)*(\s*#{@method}\s*:\s*\(.*\)\s*-\>.*$)/)
+      end
+
+    end
+  end
+end
+
+
+if __FILE__==$0
+  pry
+end
---
+++ lib/luca/documentation/documentation.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+
+if __FILE__==$0
+  require 'pry'
+end
+
+module Luca
+  module Documentation
+    COMMENTS_REGEX =  /(^\s*#.*$\n)*/
+    ARGUMENTS_REGEX = /^(.*)=(.+)/
+
+
+    def self.root
+      "#{File.expand_path '../../../..', __FILE__}"
+    end
+
+    def self.find_file component_name
+      if component_name.downcase != component_name
+        component_name = underscore component_name
+      end
+      matching_files = Dir.glob("#{root}/app/assets/javascripts/**/#{component_name}.coffee")
+      if matching_files.count == 1
+        return matching_files[0]
+      else
+        raise "Multiple possible matches: #{matching_files}"
+      end
+    end
+
+    def self.underscore string
+      string.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+    end
+
+  end
+end
+
+if __FILE__==$0
+  pry
+end
