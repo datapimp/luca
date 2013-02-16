fba4c1ccd09ed7e7ca00cca02b96a58237104415
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-02-16 03:39:04 -0600

    adding vendor folder from master to serve as an asset cache

---
+++ vendor/assets/javascripts/luca-spec.js
@@ -0,0 +1,11 @@
+//= require ./src/dependencies
+//= require ./src/index
+//= require_self
+//= require ./spec/javascripts/dependencies
+//= require_tree ./spec/javascripts/core
+//= require_tree ./spec/javascripts/concerns
+//= require_tree ./spec/javascripts/components
+//= require_tree ./spec/javascripts/containers
+//= require_tree ./spec/javascripts/managers
+
+Luca.define.close();
