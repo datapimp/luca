cf9ee55958301c371f262efae150f87557197c92
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-02-17 02:58:26 -0600

    rebuilding site

--- javascripts/docs/views/pages/home.js
+++ javascripts/docs/views/pages/home.js
@@ -1,1 +1,1 @@
-(function(){var e;e=Docs.register("Docs.views.Home"),e["extends"]("Luca.components.Page"),e.configuration({layout:"layouts/main",regions:{left:"left_navigation",right:"pages/home"}}),e.register()}).call(this);
+(function(){var e;e=Docs.register("Docs.views.Home"),e["extends"]("Luca.components.Page"),e.configuration({layout:"layouts/main",regions:{left:"left_navigation",right:"pages/home"}}),e.defines({index:function(){return this.trigger("index")}}),e.register()}).call(this);
